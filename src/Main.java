fcf174522c086c74c8930434114442a8c994b938
Radclyf <shanksakagame66@gmail.com>
2023-01-15 14:01:42 +0800

    MILESTONE A: Show the GUI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        frame.setTitle("GUI");
+        frame.setSize(500, 500);
+        frame.setVisible(true);
     }
 }
